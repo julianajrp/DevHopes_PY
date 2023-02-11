b6d9ffeabb7e2dfc3d1b1f69a3d8fc14a1d6853f
jrpjuliana <rocha.juliana.pereira@gmail.com>
2023-02-11 01:06:44 -0300

    fix:allowed_hosts

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn _dev_hopes.wsgi --log-file -
--- _dev_hopes/settings.py
+++ _dev_hopes/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-xpsx-q_6uragy&gj8tbo0zqj+ozfge($f=im=47)o*-6($_s_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dev-hopes-webb.onrender.com"]
 
 
 # Application definition
@@ -120,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = "/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
